2d5bda076674a8d15227ed7e976dee5d6847db66
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 20:24:18 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import paho.mqtt.client as paho
+import streamlit as st
+import json
+
+broker = "broker.mqttdashboard.com"
+port = 1883
+client = paho.Client("StreamlitApp")
+
+def on_publish(client, userdata, result):
+    print("El dato ha sido publicado\n")
+
+def send_message(topic, payload):
+    client.connect(broker, port)
+    client.on_publish = on_publish
+    message = json.dumps(payload)
+    ret = client.publish(topic, message)
+
+st.title("Control de Dispositivo")
+
+if st.button("Encender Dispositivo"):
+    send_message("cmqtt_And", {"Act1": "ON"})
+    st.success("El dispositivo ha sido encendido.")
+
+if st.button("Apagar Dispositivo"):
+    send_message("cmqtt_And", {"Act1": "OFF"})
+    st.success("El dispositivo ha sido apagado.")
