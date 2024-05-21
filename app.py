fb39a36f7aba8eeba0f2dcd4927e67889db470e2
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 21:03:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,27 @@ import json
 
 broker = "broker.mqttdashboard.com"
 port = 1883
-client1 = paho.Client("APP_CERR")
+client1 = paho.Client("WokwiControl")
 
 def on_publish(client, userdata, result):
     print("El dato ha sido publicado\n")
 
 def send_message(topic, payload):
-    client1.connect(broker, port)  # Utiliza client1 en lugar de client
+    client1.connect(broker, port)
     client1.on_publish = on_publish
     message = json.dumps(payload)
-    ret = client1.publish(topic, message)  # Utiliza client1 en lugar de client
+    ret = client1.publish(topic, message)
 
 st.title("Control de Dispositivo")
 
-if st.button("Encender Dispositivo"):
+if st.button('Encender Luz'):
     send_message("cmqtt_And", {"gesto": "prender luces"})
 
-if st.button("Apagar Dispositivo"):
+if st.button('Apagar Luz'):
     send_message("cmqtt_And", {"gesto": "apagar luces"})
+
+values = st.slider('Selecciona el rango de valores', 0.0, 100.0)
+st.write('Values:', values)
+
+if st.button('Enviar valor analógico'):
+    send_message("cmqtt_V", {"Analog": float(values)})
