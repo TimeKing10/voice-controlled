7d07b79c5ff063aa8e6448f3aa55c75d8d514253
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 21:47:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ client1 = paho.Client("StreamlitApp")
 def on_publish(client, userdata, result):
     print("El dato ha sido publicado\n")
 
-def send_message(topic, payload):
+def send_message(topic, gesture):
     client1.connect(broker, port)
     client1.on_publish = on_publish
-    message = json.dumps(payload)
+    message = json.dumps({"gesto": gesture})
     ret = client1.publish(topic, message)
 
 st.title("Control de Luces")
 
 if st.button('Encender'):
-    send_message("cmqtt_And", {"gesto": "ON"})
+    send_message("cmqtt_And", "ON")
     st.success("El LED ha sido encendido.")
 
 if st.button('Apagar'):
-    send_message("cmqtt_And", {"gesto": "OFF"})
+    send_message("cmqtt_And", "OFF")
     st.success("El LED ha sido apagado.")
