bd5147aa7029d15ba410da65cca441a9ba45c114
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 21:18:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as paho
 import streamlit as st
 import json
 
 broker = "broker.mqttdashboard.com"
 port = 1883
-topic = "cmqtt_And"
-client_id = "StreamlitApp"
+client1 = paho.Client("StreamlitApp")
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Conectado al broker MQTT")
-    else:
-        print("Error de conexión: ", rc)
+def on_publish(client, userdata, result):
+    print("El dato ha sido publicado\n")
 
-def on_publish(client, userdata, mid):
-    print("Mensaje publicado con éxito")
-
-def send_message(gesture):
-    try:
-        client = mqtt.Client(client_id)
-        client.on_connect = on_connect
-        client.on_publish = on_publish
-        client.connect(broker, port)
-        message = json.dumps({"gesto": gesture})
-        client.publish(topic, message)
-        client.disconnect()
-    except Exception as e:
-        print("Error al enviar el mensaje MQTT:", e)
+def send_message(topic, payload):
+    client1.connect(broker, port)
+    client1.on_publish = on_publish
+    message = json.dumps(payload)
+    ret = client1.publish(topic, message)
 
 st.title("Control de Dispositivo")
 
-if st.button("Encender Luz"):
-    send_message("prender luz")
-    st.success("La luz ha sido encendida.")
+if st.button('ON'):
+    send_message("cmqtt_And", {"Act1": "ON"})
+    st.success("El dispositivo ha sido encendido.")
 
-if st.button("Apagar Luz"):
-    send_message("apagar luz")
-    st.success("La luz ha sido apagada.")
-
+if st.button('OFF'):
+    send_message("cmqtt_And", {"Act1": "OFF"})
+    st.success("El dispositivo ha sido apagado.")
